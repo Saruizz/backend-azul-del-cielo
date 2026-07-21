cf3ba29ef8e61b9666aebdae1316ac2fb127213e
Saruizz <saruizp.dev@gmail.com>
2026-07-21 02:18:36 -0500

    (configurar)(core):configuracion de archivos estaticos, cors e importacion de modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AuditModule } from './audit/audit.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductsModule, 
+    PrismaModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    CategoriesModule,
+    AuthModule,
+    UsersModule,
+    AuditModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
