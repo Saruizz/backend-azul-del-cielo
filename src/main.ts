03af56fce966b9ae779e879fa001879f6a4ec456
Saruizz <saruizp.dev@gmail.com>
2026-07-29 05:20:06 -0500

    (actualizar)(cors):permitir configurar el origen autorizado mediante variable de entorno

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'https://azuldelcielo.saruizp.com',
+      process.env.FRONTEND_URL || 'https://azuldelcielo.saruizp.com',
       'http://localhost:4200',
       'http://localhost:3000'
     ],
     credentials: true,
-  }); // Habilitar peticiones desde el frontend oficial y desarrollo
+  }); // Habilitar peticiones desde el frontend oficial (o variable FRONTEND_URL) y desarrollo
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
